d101448f3a49cc7ec83a573e55d3d58d1805b777
lkb <lkb0398@nate.com>
2015-09-08 11:21:40 +0900

    달력 예제 -BaseAdapter을 상속받은 CalendarAdapter의 기본 와꾸

--- app/src/main/java/com/lkb0398nate/androidexam/Calendar/CalendarAdapter.java
+++ app/src/main/java/com/lkb0398nate/androidexam/Calendar/CalendarAdapter.java
@@ -1,8 +1,42 @@
 
 package com.lkb0398nate.androidexam.Calendar;
 
-/**
- * Created by kb on 2015-09-08.
- */
-public class CalendarAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.Calendar;
+import java.util.List;
+
+public class CalendarAdapter extends BaseAdapter {
+
+    private List<Calendar> mList;
+    private Context mContext;
+
+    public CalendarAdapter(Context context, List<Calendar> list) {
+
+        mList = list;
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return mList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
 }
